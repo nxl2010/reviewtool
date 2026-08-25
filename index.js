7ef457583fa828f30323b8f40b069c9e636b5937
nxl2010 <nxl2010@gmail.com>
2026-08-25 09:31:02 +0700

    Configure Worker asset fetch in index.js and wrangler.json

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 export default {
   async fetch(request, env, ctx) {
-    if (env.ASSETS) {
-      return env.ASSETS.fetch(request);
-    }
-    return new Response("Kuchen Review Tool Worker Active");
+    return env.ASSETS.fetch(request);
   }
 };
